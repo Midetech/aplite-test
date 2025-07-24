0062bb1eb31536845b88cc9f5f4dcead4e130a33
Midecodes <faginorish@gmail.com>
2025-07-25 00:06:30 +0100

    Add page for dispalying vendor directory'

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { VendorDirectory } from "@/components/VendorDirectory";
+import "@/index.css";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <VendorDirectory />
+    </div>
+  );
+};
+
+export default Index;
